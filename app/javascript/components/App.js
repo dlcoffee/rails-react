067988d287f8e469b297f59c7a20f315144cb6ce
daniel tea <daniel.l.tea@gmail.com>
2021-09-21 13:28:41 -0700

    mess around with scope

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,15 +3,37 @@ import angular from 'angular'
 
 /** Angular.js code
  */
+const angularControllers = angular.module('angularControllers', [])
 
-const angularApp = angular.module('angularApp', [])
-
-angularApp.controller('appController', [
+angularControllers.controller('angularController1', [
+  '$rootScope',
   '$scope',
-  function ($scope) {
+  function ($rootScope, $scope) {
     $scope.text = 'angular input'
+
+    $scope.onClick = () => {
+      console.log('ctrl1 click')
+      $rootScope.$emit('onClick', { data: $scope.text })
+    }
   },
 ])
+
+angularControllers.controller('angularController2', [
+  '$rootScope',
+  '$scope',
+  function ($rootScope, $scope) {
+    // $scope.$on('onClick', (event, args) => {
+    //   console.log(event, args)
+    // })
+
+    $rootScope.$on('onClick', (event, args) => {
+      $scope.text = args.data
+      // console.log(event, args)
+    })
+  },
+])
+
+const angularApp = angular.module('angularApp', ['angularControllers'])
 
 angularApp.run([
   '$rootScope',
@@ -22,7 +44,9 @@ angularApp.run([
 ])
 
 const template = `
-  <div ng-controller="appController">
+<div>
+  <h2>{{ heading }}</h2>
+  <div ng-controller="angularController1">
     <label htmlFor="ng-input">Type something here: </label>
     <input
       type="text"
@@ -30,7 +54,13 @@ const template = `
       id="ng-input"
       ng-model="text"
     ></input>
+    <button ng-click="onClick()">Submit</button>
   </div>
+
+  <div ng-controller="angularController2">
+    <h4>{{ text }}</h4>
+  </div>
+</div>
 `
 
 /** React code
@@ -42,7 +72,12 @@ const App = () => {
 
   React.useEffect(() => {
     const $injector = angular.bootstrap(ngAppRef.current, ['angularApp'])
-    console.log('useEffect $_rootscope:', $injector.get('$rootScope'))
+    const $rootScope = $injector.get('$rootScope')
+
+    $rootScope.heading = 'This is an AngularJS application'
+    $rootScope.$digest()
+
+    console.log('useEffect $_rootscope:', $rootScope)
 
     return () => {
       console.log('destroying angular scope')
